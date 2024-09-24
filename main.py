ebf1524f2c1f6fd643387b37b2989f58a3edd844
4rjunc <arjunmundamani@gmail.com>
2024-09-24 15:18:50 +0530

    Added /value command
    Responds with 1BTC value in INR

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import requests
 import logging
 import random
 import telebot
 from configuration.config import API_TOKEN
 import utils.logger as logger_save
-from telebot.types import InputFile, InputMediaDocument
+from telebot.types import InputFile, InputMediaDocument, ChatMemberUpdated
 
 # Configuring logging
 logging.basicConfig(level=logging.INFO)
@@ -15,18 +16,6 @@ logger_save.init_logger(f"logs/botlog.log")
 
 # Initializing bot with 4 thread workers for handling multiple requests
 bot = telebot.TeleBot(API_TOKEN, threaded=True, num_threads=4)
-
-# Dictionary to store user information
-user_dict = {}
-
-
-# User class to handle user details
-class User:
-    def __init__(self, name):
-        self.name = name
-        self.reg_no = None
-        self.dob = None
-        self.aadhar_no = None
 
 
 EMOJIS = [
@@ -93,20 +82,56 @@ def gm(message):
 @bot.message_handler(commands=["whitepaper"])
 def white_paper(message):
     chat_id = message.chat.id
-    # Log when user requests the whitepaper
-    logger.info(f"Sending whitepaper to user with chat_id: {chat_id}")
+    # log when user requests the whitepaper
+    logger.info(f"sending whitepaper to user with chat_id: {chat_id}")
 
     try:
         bot.send_document(
             chat_id,
-            "BQACAgUAAxkDAAMsZvGtrPJDrFCkiZjBgP5M93Zg6t8AArURAAK-oZBXwZfTpYId1RE2BA",
+            "bqacaguaaxkdaamszvgtrpjdrfckizjbgp5m93zg6t8aaruraak-ozbxwzftpyid1re2ba",
         )
-        logger.info("Whitepaper sent successfully")
-    except Exception as e:
-        logger.error(f"Failed to send whitepaper: {e}")
+        logger.info("whitepaper sent successfully")
+    except exception as e:
+        logger.error(f"failed to send whitepaper: {e}")
 
 
-# /register command handler
+# /value : shows realtime value of BTC in INR
+@bot.message_handler(commands=["value"])
+def value(message):
+    chat_id = message.chat.id
+    # log when user requests the value
+    logger.info(f"sending btc value to user with chat_id: {chat_id}")
+    try:
+        url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin/markets/?quotes=INR"
+        response = requests.get(url).json()
+        print("response", type(response))
+        inr_value = response[0]["quotes"]["INR"]["price"]
+        print("INR VALUE:", inr_value)
+        formatted_value = (
+            f"{inr_value:,.2f}"  # Adds commas and rounds to 2 decimal places
+        )
+        bot.send_message(chat_id, f"1 BTC = ₹{formatted_value}")
+    except exception as e:
+        logger.error(f"failed to fetch btc value: {e}")
+
+
+# welcome
+@bot.chat_member_handler()
+def on_c(c: ChatMemberUpdated):
+    chat_id = c.chat.id
+    print("c in on_c:", c)
+    if (
+        c.new_chat_member
+        and c.new_chat_member.status == "member"
+        and (
+            not c.old_chat_member
+            or (c.old_chat_member and c.old_chat_member.status == "kicked")
+        )
+    ):
+        bot.send_message(chat_id, f"gm, new member joined")
+
+
+# welcome message like in discord
 # /social shares social links
 # /askme [FAQ] use AI/ML if required
 # /venue : venue details
@@ -114,6 +139,7 @@ def white_paper(message):
 # /speakers : list of speakers
 # /workshops : details of workshops
 # /hackathon : hackathon details
+# /inspire: share a inspire quote use external api
 
 # Enable saving next step handlers to file "./.handlers-saves/step.save"
 bot.enable_save_next_step_handlers(delay=2)
